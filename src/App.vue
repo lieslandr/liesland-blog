063465b729ea1cc8171a909678d87744499ada6a
Rob Liesland <lieslandr@xavier.edu>
2022-01-03 11:59:16 -0600

    adds flash message.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
   <h1><router-link to="/">The Lieslands</router-link></h1>
   <h2>Simply Livin' It Up</h2>
   <div id="app">
-    <!--<div id="nav">
-      <router-link :to="{ name: 'EventList' }">Events</router-link> |
-      <router-link :to="{ name: 'PostList' }">Posts</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-    </div>-->
+     <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  inject: ['GStore'] // <---- 
+}
+</script>
 
 <style>
 #app {
@@ -35,5 +40,19 @@
 
 h4 {
   font-size: 20px;
+}
+
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
 }
 </style>
--- src/views/post/Comment.vue
+++ src/views/post/Comment.vue
@@ -6,9 +6,16 @@
 <script>
 export default {
   props: ['post'],
+  inject: ['GStore'], // <---- Inject the Global Store
   methods: {
     comment() {
       // If registration API call is successful
+
+      this.GStore.flashMessage =
+        'You have successfully commented on ' + this.post.title.rendered
+      setTimeout(() => {  // After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
       this.$router.push({
         name: 'PostDetails'
       })
